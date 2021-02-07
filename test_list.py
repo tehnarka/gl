5c7bd7db15d619e2cbf91da6be9b6bfe6786ff7c
Roman Tkalenko <rmn.tk.ml@gmail.com>
2021-02-07 19:45:20 +0200

    add basic ci check

--- test_list.py
+++ test_list.py
@@ -3,16 +3,10 @@ import unittest
 
 class TestListMethods(unittest.TestCase):
 
-    def test_len(self):
+    def test_roman_tkalenko_fi_13(self):
         self.assertEqual(len([]), 0)
         self.assertEqual(len(['a']), 1)
         self.assertEqual(len(['a', 'b']), 2)
-
-    def test_empty_check(self):
-        self.assertFalse([])
-        self.assertTrue(not [])
-        self.assertTrue(['a'])
-        self.assertFalse(not ['a'])
 
 
 if __name__ == '__main__':
