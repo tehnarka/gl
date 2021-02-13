5eb05650c800755ff344ce174c816fc484e563e1
Roman Tkalenko <tkroman@users.noreply.github.com>
2021-02-13 12:01:25 +0200

    add another test (#1)
    assert that 2 == 2

--- test_list.py
+++ test_list.py
@@ -8,6 +8,8 @@ class TestListMethods(unittest.TestCase):
         self.assertEqual(len(['a']), 1)
         self.assertEqual(len(['a', 'b']), 2)
 
+    def test_roman_tkalenko_2(self):
+        self.assertEqual(2, 2)
 
 if __name__ == '__main__':
     unittest.main()
