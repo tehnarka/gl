26ccb1c9d730ae94fb68ec922df7c36d77a0318d
Illia Kripaka <30872146+ikripaka@users.noreply.github.com>
2021-02-19 20:59:14 +0200

    Add another unit test (#11)
    * feat: add new test with list
    
    * fix: correct elements in array
    
    * fix: correct flake 8 syntax
    
    * fix: correct function and changed assertion
    
    * fix: correct incorrect syntax
    
    * docs: add contributor 'ikripaka'
    
    * fix: add spaces after comas
    
    * fix: add space after function
    
    Co-authored-by: Roman Tkalenko <tkroman@users.noreply.github.com>

--- test_list.py
+++ test_list.py
@@ -11,5 +11,9 @@ class TestListMethods(unittest.TestCase):
     def test_roman_tkalenko_2(self):
         self.assertEqual(2, 2)
 
+    def test_illia_kripaka_fi_94_2(self):
+        self.assertEqual(2*[1, 3, 5], [1, 3, 5, 1, 3, 5])
+
+
 if __name__ == '__main__':
     unittest.main()
