43e32de4049c5d30dc99c9aee4acd8e269194eb6
PulyaPolya <polinaitsme@gmail.com>
2021-02-19 19:53:57 +0200

    fix #4

--- test_list.py
+++ test_list.py
@@ -8,8 +8,9 @@ class TestListMethods(unittest.TestCase):
         self.assertEqual(len(['a']), 1)
         self.assertEqual(len(['a', 'b']), 2)
 
-    def test_roman_tkalenko_2(self):
-        self.assertEqual(2, 2)
-
+    def test_polina_kozarovitskaya(self):
+        l= [1,2,3]
+        l.clear()
+        self.assertEqual(len(l),1)
 if __name__ == '__main__':
     unittest.main()
