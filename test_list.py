409e678809959df815fbe1d45f36944f92c3e30f
Liub-ov <79312905+Liub-ov@users.noreply.github.com>
2021-02-20 18:23:43 +0200

    close Issue27 (#39)
    * add test for swap
    
    * final test
    
    * Update test_list.py
    
    * Update test_list.py
    
    * Update test_list.py
    
    * Update test_list.py
    
    * Update test_list.py
    
    * Update test_list.py
    
    * Update test_list.py
    
    * Update test_list.py
    
    * Update test_list.py
    
    * Update test_list.py
    
    * Update test_list.py
    
    * Update test_list.py
    
    * Update test_list.py

--- test_list.py
+++ test_list.py
@@ -1,12 +1,20 @@
 import unittest
 
 
+def swap_first_last_elements(list):
+    list[0], list[-1] = list[-1], list[0]
+    return list
+
+
 def rem_last_element(list):
     list.remove(list[-1])
     return list
 
 
 class TestListMethods(unittest.TestCase):
+    def test_liubov_peleshenko_fi_94(self):
+        list = [1, 2, 3, 4, 5, 6]
+        self.assertEqual(swap_first_last_elements(list), [6, 2, 3, 4, 5, 1])
 
     def test_roman_tkalenko_fi_13(self):
         self.assertEqual(len([]), 0)
