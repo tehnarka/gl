71e5a8ccce05ba399b097be074d160a7eead30c9
De1m0s114 <61823460+De1m0s114@users.noreply.github.com>
2021-02-20 14:51:02 +0200

    Close Issue22 (#47)
    * add test for remove
    with '#' will be ignored, and an empty message aborts the commit.
    
    * final test
    
    * final test
    
    * change flake8
    
    * change flake8(2)
    
    * flake8(3)
    
    * Update test_list.py
    
    * Update test_list.py
    
    * Update test_list.py
    
    * Update CONTRIBUTORS
    
    * Update CONTRIBUTORS

--- test_list.py
+++ test_list.py
@@ -1,6 +1,11 @@
 import unittest
 
 
+def rem_last_element(list):
+    list.remove(list[-1])
+    return list
+
+
 class TestListMethods(unittest.TestCase):
 
     def test_roman_tkalenko_fi_13(self):
@@ -10,6 +15,10 @@ class TestListMethods(unittest.TestCase):
 
     def test_roman_tkalenko_2(self):
         self.assertEqual(2, 2)
+
+    def test_Yegor_Panasuk_FI94(self):
+        list = [1, 2, 3, 4, 5, 6]
+        self.assertEqual(rem_last_element(list), [1, 2, 3, 4, 5])
 
     def test_michael_medved_fi93(self):
         self.assertEqual(len([] + ['f']), len('f'))
