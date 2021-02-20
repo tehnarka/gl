f9b6b853f6ef65440baf14cbfc97df9c51c13b74
PulyaPolya <polinaitsme@gmail.com>
2021-02-20 09:10:34 +0200

    fix #4

--- test_list.py
+++ test_list.py
@@ -3,11 +3,6 @@ import unittest
 
 class TestListMethods(unittest.TestCase):
 
-    def test_roman_tkalenko_fi_13(self):
-        self.assertEqual(len([]), 0)
-        self.assertEqual(len(['a']), 1)
-        self.assertEqual(len(['a', 'b']), 2)
-
     def test_polina_kozarovitskaya(self):
         t = [1, 2, 3]
         t.clear()
