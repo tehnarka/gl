3717c3d18cda9a93093dfb9b1800870f1384a88c
Michael <edd4599@gmail.com>
2021-02-20 09:43:16 +0200

    17 fix (#19)
    * 17 fix
    i
    
    * 17 fixed 1.1
    
    * 17 fix17 fix17 fix17 fix17 fix17 fix17 fix17 fix17 fix17 fix17 fix17 fix17 fix17 fix17 fix17 fix17 fix
    
    * 17
    
    * added myself to contributors
    
    * Update test_list.py
    
    Co-authored-by: MacBook Air <macbookair@MacBook-Air-MacBook.local>

--- test_list.py
+++ test_list.py
@@ -11,6 +11,9 @@ class TestListMethods(unittest.TestCase):
     def test_roman_tkalenko_2(self):
         self.assertEqual(2, 2)
 
+    def test_michael_medved_fi93(self):
+        self.assertEqual(len([] + ['f']), len('f'))
+
     def test_illia_kripaka_fi_94_2(self):
         self.assertEqual(2*[1, 3, 5], [1, 3, 5, 1, 3, 5])
 
