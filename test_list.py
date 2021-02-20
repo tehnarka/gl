8cf61951132b1e8e7d5d854c1277993b399fc50c
PulyaPolya <79004071+PulyaPolya@users.noreply.github.com>
2021-02-20 16:52:44 +0200

    #4 fix (#21)
    * fix #4
    
    * fix #4
    
    * fix #4
    
    * fix #4
    
    * fix #4
    
    * fix #4
    
    * fix #4
    
    * fix #4
    
    * fix #4
    
    * fix #4
    
    * fix #4
    
    * fix #4
    
    * fix #4
    
    * fix #4
    
    * fix #4
    
    * fix #4

--- test_list.py
+++ test_list.py
@@ -13,6 +13,11 @@ class TestListMethods(unittest.TestCase):
         self.assertEqual(len(['a']), 1)
         self.assertEqual(len(['a', 'b']), 2)
 
+    def test_polina_kozarovitskaya(self):
+        t = [1, 2, 3]
+        t.clear()
+        self.assertEqual(len(t), 0)
+
     def test_roman_tkalenko_2(self):
         self.assertEqual(2, 2)
 
