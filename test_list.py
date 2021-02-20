c7d77d6715a5e5ed5da1247e8e2085a40fd7d20a
marishka <martynovalala@gmail.com>
2021-02-20 18:38:01 +0200

    Closes #43 (#49)
    * masha's func created
    
    * broken #43
    
    * broken #43
    
    * broken #43
    
    * broken #43
    
    * broken #43
    
    * Closes #43
    
    * Closes #43
    
    * closes #43 odd test
    
    * contributors updated

--- test_list.py
+++ test_list.py
@@ -39,6 +39,10 @@ class TestListMethods(unittest.TestCase):
     def test_illia_kripaka_fi_94_2(self):
         self.assertEqual(2 * [1, 3, 5], [1, 3, 5, 1, 3, 5])
 
+    def test_maria_martynova_fi_93(self):
+        self.assertEqual(len([0]), 1)
+        self.assertEqual(len([1, 1]), 2)
+
     def test_kostiantyn_baievskyi_fi_93(self):
         self.assertEqual([1, 2, 3] + [4, 5, 6], [1, 2, 3, 4, 5, 6])
 
