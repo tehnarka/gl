8c99baae903dac9736d4615ccae3ff3b5b925547
Koroban Olya <79316653+Cereha@users.noreply.github.com>
2021-02-23 20:01:04 +0200

    closes #9 (#13)
    * test comit
    
    * add test
    
    * add function to cut out element
    
    * corect name test and funktion
    
    * Update CONTRIBUTORS
    
    * Update CONTRIBUTORS
    
    * Update test_list.py
    
    * Update test_list.py
    
    * Update test_list.py
    
    * Update test_list.py

--- test_list.py
+++ test_list.py
@@ -1,6 +1,10 @@
 import unittest
 
 
+def get_second_to_fifth_elements(a):
+    return a[2:5]
+
+
 def delete_element(list1):
     list1.remove(list1[-4])
     return list1
@@ -104,6 +108,10 @@ class TestListMethods(unittest.TestCase):
     def test_Yelizaveta_Godovikova_FI94(self):
         list = ['a', 'k', 'j', 'y', 'r', 'o']
         self.assertEqual(del_third_element(list), ['a', 'k', 'y', 'r', 'o'])
+
+    def test_Olya_Koroban_FI94(self):
+        a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.assertEqual(get_second_to_fifth_elements(a), [3, 4, 5])
 
 
 if __name__ == '__main__':
