d5100ae3c9bd48e19d6a73bdfdeeea2c9d9808f4
Tim-Fedor <31453165+Tim-Fedor@users.noreply.github.com>
2021-02-26 11:07:36 +0200

    Closes #58 (#59)
    * Closes max test
    
    * Closes #58
    
    * Closes #58
    
    * Update test_list.py
    
    * Update test_list.py
    
    * Update test_list.py
    
    * Update CONYTIBUTORS
    
    * Update CONTRIBUTORS
    
    * Update CONTRIBUTORS

--- test_list.py
+++ test_list.py
@@ -113,6 +113,11 @@ class TestListMethods(unittest.TestCase):
         a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         self.assertEqual(get_second_to_fifth_elements(a), [3, 4, 5])
 
+    def test_tymothy_fedorchuk_fi_93(self):
+        self.assertEqual(max([1, 10, 100]), 100)
+        self.assertEqual(max([4, 12, 1]), 12)
+        self.assertEqual(max([1, 2, 3]), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
